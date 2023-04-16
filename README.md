f8e657f89fb8bca10ae79475ac2a83076f222792
EFE SAMED KIRLI <efesamed@outlook.com.tr>
2023-04-16 12:03:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<div class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+            <button class="delete"><i class="fa-solid fa-trash"></i></button>
+        </div>
+
+            <div class="main hidden"></div>    <!--hidden-->
+                
+            
+            <textarea></textarea>
+    </div>
---
+++ app.js
@@ -0,0 +1,19 @@
+
+const notesEl = document.querySelector('.notes');
+const editBtn = document.querySelector('.edit');
+const deleteBtn = document.querySelector('.delete');
+
+
+const main = notesEl.querySelector('.main');
+const textarea = notesEl.querySelector('textarea')
+
+
+editBtn.addEventListener('click',()=>{
+    notesEl.classList.toggle('hidden')
+    textarea.classList.toggle('hidden')
+})   
+
+textarea.addEventListener('input', (e) => {
+    const {value}  = e.target;
+     main.innerHTML = marked(value)  
+})
